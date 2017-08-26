7bfb90ade23ce47106bc7430b6e08a1c14c4734e
IwonaJarkiewicz <politea@gmail.com>
2017-08-26 10:16:22 +0200

    formularz_walidacja_all

--- js/validacja.js
+++ js/validacja.js
@@ -1,9 +1,27 @@
 'use strict'
-var wyslijBtn = document.getElementById('wyślij');
-if(document.getElementById('name').value.trim() == "") {
+var wyslijBtn = document.getElementById('wyslij');
+var wszystkieZgody = document.getElementById('wszystkie-zgody');
+var zgodaMarketingowa1 = document.getElementById('zgoda-marketingowa-1');
+var zgodaMarketingowa2 = document.getElementById('zgoda-marketingowa-2')
+
+
+function stanCheckbox(){
+    zgodaMarketingowa1.checked = this.checked;
+    zgodaMarketingowa2.checked = this.checked;
+    }
+
+function waliduj() {
+if (document.getElementById('imie').value.trim() == "") {
     alert('Wypełnij imię');
 }
-    
+if (document.getElementById('email').value.trim() == "") {
+    alert('Wypełnij e-mail');
+}
 
+if (!zgodaMarketingowa1.checked) {
+    alert('Zaznacz pierwszą zgodę');
+}
+}
 
-
+wyslijBtn.addEventListener('click', waliduj);
+wszystkieZgody.addEventListener('change',stanCheckbox);
